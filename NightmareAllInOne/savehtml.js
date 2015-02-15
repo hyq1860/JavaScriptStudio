b3df9d5137ba549a66b66ae8d12f1b87cd5c9a3f
hyq1860 <pchero@163.com>
2015-02-15 17:08:30 +0800

    201502151708

--- NightmareAllInOne/savehtml.js
+++ NightmareAllInOne/savehtml.js
@@ -1,14 +1,15 @@
 ﻿var fs = require('fs');
 var Nightmare = require('nightmare');
-new Nightmare({
+var web = new Nightmare({
     weak: false,
     timeout: 10000
-    })
-  .goto('http://item.jd.com/1279453.html')
+});
+web.goto('http://item.jd.com/1279453.html')
     .evaluate(function () {
         return { html: document.documentElement.innerHTML };
-    }, function (html) {
-    var temphtml = html;
-    fs.writeFileSync('./baidu.html', temphtml.html,'UTF-8');
-})
+}, function (html) {
+        var pid = web.pid();
+        var temphtml = html;
+        fs.writeFileSync('./baidu.html', temphtml.html, 'UTF-8');
+    })
 .run();
