8df42798188d1948c1b92bbaba8fadd34c8a1297
hyq1860 <pchero@163.com>
2015-03-02 18:24:42 +0800

    201503021824

--- NodeExpress/app.js
+++ NodeExpress/app.js
@@ -5,6 +5,21 @@
 var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var path = require('path');
+
+//template engine
+var ejs = require('ejs');
+//ejs.open = '{{';
+//ejs.close = '}}';
+
+//注册ejs模板为html页。简单的讲，就是原来以.ejs为后缀的模板页，现在的后缀名可以//是.html了
+app.engine('.html', require('ejs').__express);
+//设置视图模板的默认后缀名为.html,避免了每次res.Render("xx.html")的尴尬
+app.set('view engine', 'html');
+//设置模板文件文件夹,__dirname为全局变量,表示网站根目录
+app.set('views', __dirname + '\\views');
+
+app.use(express.static(__dirname + '\\public'));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -19,14 +34,24 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    var users = [
+        { name: 'tobi', email: 'tobi@learnboost.com' },
+        { name: 'loki', email: 'loki@learnboost.com' },
+        { name: 'jane', email: 'jane@learnboost.com' }
+    ];
+    //向页面模板传递参数，可以传递字符串和对象，注意格式
+    res.render('index', {
+        users: users,
+        title: "EJS example",
+        header: "Some users"
+    });
 });
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/', router);
 
 app.use('/book/:id', function(req, res, next) {
     console.log('ID:', req.params.id);
--- NodeExpress/readme.txt
+++ NodeExpress/readme.txt
@@ -3,6 +3,11 @@ https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 
 http://www.cnblogs.com/haogj/p/3985438.html
 
+淘宝的前后端分离
+https://github.com/carlisliu/modelproxy/blob/master/demo/basicUsageDemo.js
+
+http://ued.taobao.org/blog/category/bowen/frontend/
+
 https://www.meteor.com/
 
 https://github.com/struCoder/testMd/blob/master/express4.md
@@ -10,3 +15,12 @@ https://github.com/struCoder/testMd/blob/master/express4.md
 npm install express --save
 
 npm install body-parser --save
+
+//考虑自动根据webapi生成nodejs代码
+
+
+nodejs模本对比
+http://paularmstrong.github.io/node-templates/
+
+ejs
+http://www.cnblogs.com/rubylouvre/p/3421805.html
