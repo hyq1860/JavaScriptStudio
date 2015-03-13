5e3abb75266bbefb6d9cef464f2bca7e4a7b30b3
hyq1860 <pchero@163.com>
2015-03-13 18:18:31 +0800

    201503131818

---
+++ NodeFoundation/jd-book/jdbook.js
@@ -0,0 +1,8 @@
+﻿//npm install jd-book
+var jdBook = require('jd-book');
+var url = 'http://item.jd.com/958912.html';
+var show = function (data) {
+    if (!data.err)
+        console.log(data.book);
+}
+jdBook.getbook(url, show);
