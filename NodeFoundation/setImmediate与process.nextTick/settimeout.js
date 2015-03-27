c5301af8ede79e02dcd28e79525506ab4841238a
hyq1860 <pchero@163.com>
2015-03-27 18:10:23 +0800

    201503071810

---
+++ NodeFoundation/setImmediate与process.nextTick/settimeout.js
@@ -0,0 +1,19 @@
+﻿/*
+console.log(1);
+setTimeout(function() {
+    console.log(2);
+}, 1000);
+console.log(3);
+*/
+
+console.log(4);
+setTimeout(function () {
+    console.log(5);
+}, 0);
+console.log(6);
+
+
+process.nextTick(function() {
+    console.log(7);
+    process.nextTick(function() {console.log(8)});
+})
