5044f7ac26509f6f5ab0011e460dc3935e2d9180
hyq1860 <pchero@163.com>
2015-08-20 00:09:03 +0800

    nw

---
+++ NightmareAllInOne/stock.js
@@ -0,0 +1,39 @@
+﻿var Nightmare = require('nightmare');
+
+var myScrape = new Nightmare(
+    {
+        loadImages: false,
+        weak: false,
+        timeout: 100,
+        proxy: '127.0.0.1:8087',
+        output_encoding: 'gb2312'
+        //phantomPath:'D:\\Sync\\Node\\phantomjs-1.9.8-windows\\'
+    }
+);
+
+
+myScrape
+    .goto("http://quote.eastmoney.com/f1.html?code=000002&market=2")
+    .evaluate(function () {
+        return jsTimeSharingData;
+}, function (result) {
+    for (var i=0; i < result.data.length; i++) {
+        var array = result.data[i].split(',');
+        console.log(array[0]);
+        console.log(array[1]);
+        console.log(array[2]);
+        console.log(array[3]);
+    }
+        
+});
+//http://quotes.money.163.com/trade/cjmx_000002.html#06d02
+//myScrape
+//    .goto("http://hqdigi2.eastmoney.com/EM_Quote2010NumericApplication/CompatiblePage.aspx?Type=OB&stk=0000022&Reference=xml&limit=0&page=1&rt=0.9733460071005027")
+//    .evaluate(function () {
+//    return { html: document };
+//}, function (result) {
+//    console.log(result);
+//});
+
+
+myScrape.run();
--- NodeFoundation/request/r03.js
+++ NodeFoundation/request/r03.js
@@ -5,15 +5,18 @@ var request = require('request');
 var qs = require('querystring');
 var iconv = require('iconv-lite');
 var cheerio = require('cheerio');
-
+var JSON2 = require('JSON2');
 var response = function (err, response, body) {
     //返回的body 直接就是buffer 了...
     var html = iconv.decode(body, 'utf-8');
     console.log(html);
-
+    html = html.replace("var jsTimeSharingData=", "");
+    html = html.replace(";","");
+    var jsonData = JSON2.parse(html);
+    console.log(jsonData);
 }
 
 request.get({
-    url : "http://127.0.0.1:8080" ,
+    url : "http://hqdigi2.eastmoney.com/EM_Quote2010NumericApplication/CompatiblePage.aspx?Type=OB&stk=0000022&Reference=xml&limit=0&page=1&rt=0.9733460071005027" ,
     encoding : null //让body 直接是buffer
 }, response);
