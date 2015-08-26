8bed79596d66af2b843fc23ea55c73dc5aa762f1
hyq1860 <pchero@163.com>
2015-08-26 17:40:44 +0800

    fetch

--- NWStudio/node/szpe.js
+++ NWStudio/node/szpe.js
@@ -10,6 +10,9 @@ var qs = require('querystring');
 var iconv = require('iconv-lite');
 var cheerio = require('cheerio');
 var mysqldao = require('./mysqldao');
+var fetch = require('node-fetch');
+
+
 var pageNumbers = [];
 for (var i = 1; i <= 1; i++) {
     pageNumbers.push(i);
@@ -27,6 +30,16 @@ async.eachSeries(pageNumbers, function (item, callback) {
             console.log(error);
         }
     });
+
+    fetch('http://www.szse.cn/szseWeb/FrontController.szse?ACTIONID=8&CATALOGID=1815_stock_child&txtDm=000002&ENCODE=1&TABKEY=tab1')
+    .then(function (res) {
+        return res.text();
+    }).then(function (body) {
+        console.log(body);
+    });
+
+
+
 
 }, function (err) {
     if (err) {
